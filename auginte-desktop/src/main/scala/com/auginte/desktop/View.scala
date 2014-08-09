09eb757cea0fdea6af3544226fbb09ecf4747678
Aurelijus Banelis <aurelijus@banelis.lt>
2014-08-09 20:24:51 +0300

    Unstable: Desktop: Context menu for View.

--- auginte-desktop/src/main/scala/com/auginte/desktop/View.scala
+++ auginte-desktop/src/main/scala/com/auginte/desktop/View.scala
@@ -2,7 +2,9 @@ package com.auginte.desktop
 
 import javafx.collections.ObservableList
 
+import com.auginte.desktop.events.ShowContextMenu
 import com.auginte.desktop.nodes.MouseFocusable
+import scala.compat.Platform
 import scalafx.Includes._
 import com.auginte.desktop.rich.RichSPane
 import javafx.scene.layout.{Pane => jp}
@@ -11,6 +13,8 @@ import com.auginte.desktop.actors.{ZoomableView, DragableView, Container}
 import com.auginte.desktop.zooming.ZoomableCamera
 import scalafx.animation.Timeline
 import javafx.animation.KeyFrame
+import scalafx.application.Platform
+import scalafx.scene.input.{MouseButton, MouseEvent}
 import scalafx.util.Duration
 import scalafx.event.ActionEvent
 
@@ -22,7 +26,9 @@ import scalafx.event.ActionEvent
  */
 class View extends RichSPane
 with Container[jp] with DragableView[jp] with ZoomableView[jp]
-with ZoomableCamera[jp] with MouseFocusable[jp] {
+with ZoomableCamera[jp] with MouseFocusable[jp]
+with HaveOperations
+{
 
   val contextMenu = initContextMenu()
 
@@ -47,4 +53,21 @@ with ZoomableCamera[jp] with MouseFocusable[jp] {
   }
 
   def remove(element: Node): Unit = content.remove(element)
+
+
+  //
+  // Operations
+  //
+
+  mouseClicked += {
+    (e: MouseEvent) => if (e.button == MouseButton.SECONDARY) {
+      view ! ShowContextMenu(this)
+    }
+  }
+
+  override def operations: Operations = Map(
+    "Open" -> ((e: ActionEvent) => println("Open")),
+    "Save" -> ((e: ActionEvent) => println("Save")),
+    "Exit" -> ((e: ActionEvent) => HelloScalaFX.quit())
+  )
 }
