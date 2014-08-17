6620832591db5797259db0e5525d6f277ea845e3
Aurelijus Banelis <aurelijus@banelis.lt>
2014-08-17 14:44:36 +0300

    Unstable: Distribution: Unique 32 length ids

--- auginte-distribution/src/main/scala/com/auginte/distribution/data/Data.scala
+++ auginte-distribution/src/main/scala/com/auginte/distribution/data/Data.scala
@@ -1,10 +1,18 @@
 package com.auginte.distribution.data
 
 /**
- * Marker for objects, that can be saved.
+ * Common functionality for objects, that can be saved.
  *
  * @author Aurelijus Banelis <aurelijus@banelis.lt>
  */
 trait Data {
+  private val random1 = Math.random() * 1e4
+  private val random2 = Math.random() * 1e4
+  private val random3 = Math.random() * 1e4
+  private val uniqueId = f"${System.nanoTime}%h$random1%h$random2%h$random3%h"
 
+  /**
+   * @return unique 32 chars size lower case hex id. E.g. 2cb1a090dd08aecffdb51561ab2b7200
+   */
+  def id: String = f"$uniqueId%-32s".replaceAll(" ", "0")
 }
---
+++ auginte-distribution/src/test/scala/com/auginte/distribution/data/DataSpec.scala
@@ -0,0 +1,29 @@
+package com.auginte.distribution.data
+
+import com.auginte.test.UnitSpec
+
+/**
+ * Unit test for [[com.auginte.distribution.data.Data]]
+ *
+ * @author Aurelijus Banelis <aurelijus@banelis.lt>
+ */
+class DataSpec extends UnitSpec {
+
+  "Data object" must {
+    "have ids of 32 chars length" in {
+      for (i <- 0 to 100) {
+        val data = new Data{}
+        assert(32 === data.id.length)
+      }
+    }
+    "generate have unique ids" in {
+      val amount = 100
+      val all = new Array[String](amount)
+      for (i <- 1 to amount) {
+        val data = new Data{}
+        all(i - 1) = data.id
+      }
+      assert(amount === all.toSet.size)
+    }
+  }
+}
