19094dc7339f3c2e0e59cc2afb7bf6c574faa547
Aurelijus Banelis <aurelijus@banelis.lt>
2015-05-24 19:34:22 +0300

    #396: Debugging connection

--- auginte-server/app/com/auginte/server/controllers/Application.scala
+++ auginte-server/app/com/auginte/server/controllers/Application.scala
@@ -39,12 +39,25 @@ object Application extends Controller {
   def store(hash: String) = Action { request =>
     request.body.asFormUrlEncoded.flatMap(m => m.get("data").flatMap(m => m.headOption)) match {
       case Some(json: String) => Unpickle[Persistable].fromString(json) match {
-        case Success(persitable) => withDB { db =>
-          val user = DatabaseStorage.storeToDb(db, persitable.withStorage(new Storage("", hash)))
-          val id = user.getId.toString.substring(1)
-          val response = Pickle.intoString(Saved(success = true, id, hash))
-          Ok(response)
-        }
+        case Success(persitable) =>
+          try {
+            withDB { db =>
+              val user = DatabaseStorage.storeToDb(db, persitable.withStorage(new Storage("", hash)))
+              val id = user.getId.toString.substring(1)
+              val response = Pickle.intoString(Saved(success = true, id, hash))
+              Ok(response)
+            }
+          } catch {
+            case e: Exception =>
+              //FIXME: debugging
+              val config = Play.application.configuration
+              println("FAILED TO CONNECT WITH:")
+              println(config.getString("orientdb.path"))
+              println(config.getString("orientdb.db.main"))
+              println(config.getString("orientdb.user"))
+              println(config.getString("orientdb.password"))
+              InternalServerError("Cannot connect to database")
+          }
         case Failure(error) => BadRequest("Malformed data: " + error.getMessage)
       }
       case None => BadRequest("No data sent")
